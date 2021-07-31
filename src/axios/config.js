60b8dd743956f9358f61a60fc7ca230817a1e05e
Jacey Huang <79238451+EtHereAlPlusor@users.noreply.github.com>
2021-07-31 13:29:19 +0800

    Delete src directory

--- src/axios/config.js
+++
@@ -1,9 +0,0 @@
-//请求配置：后端的IP及端口
-
-// 资源服务器
-export const requestIp = "http://115.29.196.107:8809";
-
-// 授权服务器
-export const authIp = "http://115.29.196.107:8085"
-
-export const downUrl= requestIp//下载报告地址
